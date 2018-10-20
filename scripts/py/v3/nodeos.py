e236a5d3fcd04a4d4bb9e4356efa78d09851538d
I am The Cheese <ch33s3codes@gmail.com>
2018-10-20 23:45:00 +0300

    Add threshold

--- scripts/py/v3/nodeos.py
+++ scripts/py/v3/nodeos.py
@@ -15,11 +15,12 @@ POST_FIXES = {
     'get_info': 'v1/chain/get_info'
 }
 default_url = 'http://127.0.0.1:8888/{}'.format(POST_FIXES.get('get_info'))
-
+default_threshold = 5.0
 
 class Monitor:
-    def __init__(self, url):
+    def __init__(self, url, threshold):
         self.url = str(url)
+        self.threshold = threshold
 
     def monitor(self):
         try:
@@ -33,25 +34,32 @@ class Monitor:
             diff = (utc_time - block_time).total_seconds()
             # data = {"block_time": raw_block_time,
             #         "current_local_time": str(now),  "difference": diff}
-            message = "OK - The Difference is {}".format(diff)
-            print(message)
-            sys.exit(0)
+            if diff <= threshold :
+                message = "OK - The Difference is {}".format(diff)
+                print(message)
+                sys.exit(0)
+            elif diff > threshold:
+                print("CRITICAL - The block time exceeded the set threshold by {}".format(diff - threshold))
+           
         except requests.exceptions.RequestException:
             exit_code, message = (2, 'WARNING - cannot connect to host {}'.format(url))
             print(message)
             sys.exit(2)
         except Exception as e:
             message = "UNKNOWN - {}".format(str(e))
-            sys.exit(2)
+            sys.exit(3)
 
 
 def usage():
     global url
+    global threshold
     parser = argparse.ArgumentParser(description='Nodeos block time monitor')
     parser.add_argument(
         '-u', '--url', default=default_url, help='host url to check')
+    parser.add_argument("-t", "--threshold", default=default_threshold,type=float, help="the threshold to check against")
     args = parser.parse_args()
     url = args.url
+    threshold = args.threshold
 
 
 def notify_telegram():
@@ -60,7 +68,7 @@ def notify_telegram():
 
 
 def main():
-    Monitor(url).monitor()
+    Monitor(url, threshold).monitor()
 
 
 if __name__ == '__main__':
